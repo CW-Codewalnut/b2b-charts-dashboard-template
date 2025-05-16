743dee88b5b671e62c54164d3c890dde102c182d
Ranjan Bhagat <101562100+ranjan-codewalnut@users.noreply.github.com>
2025-05-16 21:29:21 +0530

    fix: replace the localhost base URL with actual URL

--- charts-dashboard-template-frontend/src/config/api.ts
+++ charts-dashboard-template-frontend/src/config/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
-const API_BASE_URL: string = "http://localhost:8080";
+const API_BASE_URL: string = "https://chartsdemo-api.codewalnut.com";
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
